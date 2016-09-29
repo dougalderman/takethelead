0758d34b563227b0864ad5a8727c4864750fdf4f
Doug Alderman <agnadcp-d1@yahoo.com>
2016-09-29 17:51:34 -0600

    home screen done

--- app/js/mainCtrl.js
+++ app/js/mainCtrl.js
@@ -5,6 +5,15 @@
 
     var ctrl = this;
 
+    $(document).ready(function() {
+
+      $.localScroll();
+
+      var leftMargin = $('.pic-text h1').width() * -0.5;
+
+      $('.pic-text h1').css({'left': '50%', 'margin-left': leftMargin + 'px'});  // Set left of absolutely positioned element to 50%. Set margin left to -0.5 of width. This will center the element.
+  	});
+
     ctrl.mobileNavbarSelected = false;
 
     ctrl.activateMobileNavbar = function() {
